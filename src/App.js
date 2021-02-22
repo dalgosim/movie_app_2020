cc98545b2d583d3023015185ff6e6a7b52c7116a
dalgosim <dalgosim@gmail.com>
2021-02-22 22:31:42 +0900

    7-1. 영화앱 전체 모습 수정하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 // import PropTypes from 'prop-types';
+import Movie from './Movie';
+import './css/App.css';
 
 class App extends React.Component {
     state = {
@@ -8,27 +10,66 @@ class App extends React.Component {
         movies: [],
     };
     
-    constructor(props) {
-        //생성자
-        super(props);
+    // constructor(props) {
+    //     //생성자
+    //     super(props);
+    // }
+
+    getMovies = async () => {
+        const {
+            data: {
+                data: {movies},
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+        this.setState({movies: movies, isLoading: false});
     }
     
     componentDidMount() {
         //처음 render 후
-        axios.get('https://yts-proxy.now.sh/list_movies.json');
-        setTimeout(() => {
-            this.setState({ isLoading: false });
-        }, 2000);
+        // setTimeout(() => {
+        //     this.setState({ isLoading: false });
+        // }, 2000);
+        this.getMovies();
     }
     
     componentDidUpdate() {
         //매번 render 후
     }
+
+    loading() {
+        return (
+            <div className="loader">
+                <span className="loader__text">Loading...</span>
+            </div>
+        );
+    }
+
+    render_movies() {
+        const {isLoading, movies} = this.state;
+        return (
+            <div className="movies">
+                {movies.map((movie) => {
+                    return (
+                        <Movie
+                            key={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.medium_cover_image}
+                        />
+                    );
+                })};
+            </div>
+        );
+    }
     
     render() {
         //매번 그려질때
-        const {isLoading} = this.state;
-        return <div> {isLoading ? 'Loading...' : 'We are ready'} </div>;
+        return (
+            <section> 
+                {this.state.isLoading ? this.loading() : this.render_movies()}
+            </section>
+        );
     }
 }
 
